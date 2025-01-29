5618fec44d9382bf91e03a6082dbfc92eb905db7
Tony031201 <atuony0312@gmail.com>
2025-01-29 00:38:34 -0800

    Update_backend

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,14 @@ async function bootstrap() {
     credentials: true, // 允许跨域传递 cookie
   });
 
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
